24c4e2079ad105f63ecfe124e089852ab918ed14
Pieter Steyaert <steyaert.pieter@gmail.com>
2015-07-08 16:38:28 +0200

    replaced xml with js file with basic p5 drawing code

---
+++ code.js
@@ -0,0 +1,10 @@
+function setup() {
+  // uncomment this line to make the canvas the full size of the window
+  var myCanvas = createCanvas(600, 400);
+  myCanvas.parent('myContainer');
+}
+
+function draw() {
+  // draw stuff here
+  ellipse(width/2, height/2, 100, 50);
+}
